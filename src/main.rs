03a3b3106a338d7df45dbb6fad819280d54e6181
Lucas Weng <lucasheweng@gmail.com>
2022-08-12 23:42:35 +0800

    feat: set up connection pool for subscribers' data storage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listner = TcpListener::bind(address)?;
-    run(listner)?.await
+    run(listner, connection_pool)?.await
 }
