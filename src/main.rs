765f2c458e9f4ca65a5bf3d57524f47b87b50914
Lucas Weng <lucasheweng@gmail.com>
2022-08-15 00:27:51 +0800

    feat: compose tracing layers to collect telemetry data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let config = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", config.application_port);
