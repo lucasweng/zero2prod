8dae628a3b28c93c98e27a1198ff551dc868fbb7
Lucas Weng <lucasheweng@gmail.com>
2022-09-06 21:16:30 +0800

    refactor: configurable timeout threshold to speed up test execution time

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -43,10 +43,12 @@ async fn spawn_app() -> TestApp {
         .email_client
         .sender()
         .expect("Invalid sender email address.");
+    let timeout = config.email_client.timeout();
     let email_client = EmailClient::new(
         config.email_client.base_url,
         sender_email,
         config.email_client.authorization_token,
+        timeout,
     );
 
     let server =
