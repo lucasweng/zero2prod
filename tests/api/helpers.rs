2fc74ea369624e03e0f699ad317eba51e51ca197
Lucas Weng <lucasheweng@gmail.com>
2022-09-19 20:49:25 +0800

    feat: configurable base url of confirmation link

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -24,6 +24,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
 }
@@ -63,11 +64,12 @@ pub async fn spawn_app() -> TestApp {
 
     // Launch the application as a background task
     let application = Application::build(config.clone()).expect("Failed to build application.");
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
-        address,
+        address: format!("http://127.0.0.1:{}", application_port),
+        port: application_port,
         db_pool: get_connection_pool(&config.database),
         email_server,
     }
