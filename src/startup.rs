5e706f0ad17ddfabbc7136c9a73a5dccceb9a934
Lucas Weng <lucasheweng@gmail.com>
2023-01-09 23:27:04 +0800

    feat: logout

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,8 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::admin_dashboard;
 use crate::routes::{
-    change_password, change_password_form, confirm, health_check, home, login, login_form,
-    publish_newsletter, subscribe,
+    admin_dashboard, change_password, change_password_form, confirm, health_check, home, log_out,
+    login, login_form, publish_newsletter, subscribe,
 };
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
@@ -106,6 +105,7 @@ async fn run(
             .route("/admin/dashboard", web::get().to(admin_dashboard))
             .route("/admin/password", web::get().to(change_password_form))
             .route("/admin/password", web::post().to(change_password))
+            .route("/admin/logout", web::post().to(log_out))
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
             .app_data(db_pool.clone())
