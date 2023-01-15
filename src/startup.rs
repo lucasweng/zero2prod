2fc66c47afe3b56ff9490c38f7637a07773f14ac
Lucas Weng <lucasheweng@gmail.com>
2023-01-15 12:36:42 +0800

    refactor: use middleware to rejecy anonymous users

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use crate::authentication::reject_anonymous_users;
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
@@ -102,12 +103,16 @@ async fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/", web::get().to(home))
-            .route("/admin/dashboard", web::get().to(admin_dashboard))
-            .route("/admin/password", web::get().to(change_password_form))
-            .route("/admin/password", web::post().to(change_password))
-            .route("/admin/logout", web::post().to(log_out))
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
+            .service(
+                web::scope("/admin")
+                    .wrap(actix_web_lab::middleware::from_fn(reject_anonymous_users))
+                    .route("/dashboard", web::get().to(admin_dashboard))
+                    .route("/password", web::get().to(change_password_form))
+                    .route("/password", web::post().to(change_password))
+                    .route("/logout", web::post().to(log_out)),
+            )
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
